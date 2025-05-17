9878f6fbbc2110e1142e204b1adc36e712b7861e
Sulagna Pradhan <infinityresources81@gmail.com>
2025-05-17 12:21:20 +0530

    Changes to be committed:

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+export default {
+  darkMode: "class", // 🔥 This enables manual dark mode
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
